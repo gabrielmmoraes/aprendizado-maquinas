8beefe83b99b7f867684cd7bd6ebbaf7ee4a90ed
Gabriel Mendes <gabrielm.moraes@poli.ufrj.br>
2018-11-25 14:49:04 -0200

    Dropando tabelas de tipo e vendedor

--- src/GBM.py
+++ src/GBM.py
@@ -3,19 +3,49 @@ import pandas as pd
 dataset = pd.read_csv('../input/train.csv')
 dadosTeste = pd.read_csv("../input/test.csv")
 
-dataset.drop(["diferenciais"], axis=1)
-dadosTeste.drop(["diferenciais"], axis=1)
+dataset = dataset.drop(columns=["diferenciais", "tipo", "tipo_vendedor"], axis=1)
+dadosTeste = dadosTeste.drop(columns=["diferenciais", "tipo", "tipo_vendedor"], axis=1)
 
 X = dataset.iloc[:, 1:-1].values
 Y = dataset.iloc[:, -1].values
 
 Xteste = dadosTeste.iloc[:, 1:].values
 
+################################################################################################################
+
 # Tratando colunas numéricas
 
 import numpy as np
 
 # Tratando coluna de quartos
+
+mean = np.mean(X[:, 1])
+std = np.std(X[:, 1])
+
+X[:, 1] -= mean
+X[:, 1] /= std
+
+mean = np.mean(Xteste[:, 1])
+std = np.std(Xteste[:, 1])
+
+Xteste[:, 1] -= mean
+Xteste[:, 1] /= std
+
+# Tratando coluna de suítes
+
+mean = np.mean(X[:, 2])
+std = np.std(X[:, 2])
+
+X[:, 2] -= mean
+X[:, 2] /= std
+
+mean = np.mean(Xteste[:, 2])
+std = np.std(Xteste[:, 2])
+
+Xteste[:, 2] -= mean
+Xteste[:, 2] /= std
+
+# Tratando coluna de vagas
 
 mean = np.mean(X[:, 3])
 std = np.std(X[:, 3])
@@ -29,7 +59,7 @@ std = np.std(Xteste[:, 3])
 Xteste[:, 3] -= mean
 Xteste[:, 3] /= std
 
-# Tratando coluna de suítes
+# Tratando coluna de área útil
 
 mean = np.mean(X[:, 4])
 std = np.std(X[:, 4])
@@ -43,7 +73,7 @@ std = np.std(Xteste[:, 4])
 Xteste[:, 4] -= mean
 Xteste[:, 4] /= std
 
-# Tratando coluna de vagas
+# Tratando coluna de área extra
 
 mean = np.mean(X[:, 5])
 std = np.std(X[:, 5])
@@ -54,44 +84,10 @@ X[:, 5] /= std
 mean = np.mean(Xteste[:, 5])
 std = np.std(Xteste[:, 5])
 
-Xteste[:, 6] -= mean
-Xteste[:, 6] /= std
+Xteste[:, 5] -= mean
+Xteste[:, 5] /= std
 
-# Tratando coluna de área útil
-
-mean = np.mean(X[:, 6])
-std = np.std(X[:, 6])
-
-X[:, 6] -= mean
-X[:, 6] /= std
-
-mean = np.mean(Xteste[:, 6])
-std = np.std(Xteste[:, 6])
-
-Xteste[:, 6] -= mean
-Xteste[:, 6] /= std
-
-# Tratando coluna de área extra
-
-X[:, 7] /= 100
-mean = np.mean(X[:, 7])
-std = np.std(X[:, 7])
-
-X[:, 7] -= mean
-X[:, 7] /= std
-
-Xteste[:, 7] /= 100
-mean = np.mean(Xteste[:, 7])
-std = np.std(Xteste[:, 7])
-
-Xteste[:, 7] -= mean
-Xteste[:, 7] /= std
-
-# Coluna de diferenciais é redundante
-
-X[:, 8] = 0
-
-Xteste[:, 8] = 0
+################################################################################################################
 
 # Tratando colunas de classificação
 
@@ -106,28 +102,9 @@ colunaTreinoTeste = labelencoder.fit_transform(np.append(X[:, 0], Xteste[:, 0]))
 X[:, 0] = colunaTreinoTeste[:Xsize]
 Xteste[:, 0] = colunaTreinoTeste[:XtesteSize]
 
-Xsize = X[:, 1].size
-XtesteSize = Xteste[:, 1].size
+################################################################################################################
 
-colunaTreinoTeste = labelencoder.fit_transform(np.append(X[:, 1], Xteste[:, 1]))
-X[:, 1] = colunaTreinoTeste[:Xsize]
-Xteste[:, 1] = colunaTreinoTeste[:XtesteSize]
-
-Xsize = X[:, 2].size
-XtesteSize = Xteste[:, 2].size
-
-colunaTreinoTeste = labelencoder.fit_transform(np.append(X[:, 2], Xteste[:, 2]))
-X[:, 2] = colunaTreinoTeste[:Xsize]
-Xteste[:, 2] = colunaTreinoTeste[:XtesteSize]
-
-""" Xsize = X[:, 8].size
-XtesteSize = Xteste[:, 8].size
-
-colunaTreinoTeste = labelencoder.fit_transform(np.append(X[:, 8], Xteste[:, 8]))
-X[:, 8] = colunaTreinoTeste[:Xsize]
-Xteste[:, 8] = colunaTreinoTeste[:XtesteSize] """
-
-onehotencoder = OneHotEncoder(categorical_features = [0, 1, 2])
+onehotencoder = OneHotEncoder(categorical_features = [0])
 
 tabelaTreinoTeste = np.zeros((X.shape[0]+Xteste.shape[0], X.shape[1]))
 
@@ -137,6 +114,7 @@ for i in range(X.shape[1]):
 X = onehotencoder.fit_transform(tabelaTreinoTeste).tocsr()[:X.shape[0], :]
 Xteste = onehotencoder.fit_transform(tabelaTreinoTeste).tocsr()[X.shape[0]:, :]
 
+################################################################################################################
 
 from sklearn.model_selection import train_test_split
 
@@ -144,13 +122,15 @@ X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size = 0.33)
 
 from sklearn.ensemble import GradientBoostingRegressor
 
-regressor = GradientBoostingRegressor(n_estimators=4000, learning_rate=0.05,
-                                   max_depth=4, max_features='sqrt',min_samples_split=60)
+regressor = GradientBoostingRegressor(n_estimators=5000, learning_rate=0.01,
+                                   max_depth=4, max_features=0.4,min_samples_split=300)
 
 regressor.fit(X_train, y_train)
 
 y_pred_train = regressor.predict(X_train)
 y_pred_test  = regressor.predict(X_test )
+
+################################################################################################################
 
 import math
 from sklearn.metrics import mean_squared_error, r2_score
@@ -163,6 +143,7 @@ print('\nDesempenho no conjunto de teste:')
 print('RMSE = %.3f' % math.sqrt(mean_squared_error(y_test , y_pred_test)))
 print('R2   = %.3f' %                     r2_score(y_test , y_pred_test) )
 
+################################################################################################################
 
 Yteste = regressor.predict(Xteste)
 
